1bbd61ce50a854aceb12fe8823e7b39ce28c2424
d12frosted <d12frosted@gmail.com>
2016-07-19 18:32:05 +0300

    add functions for getting numbers from env

--- src/Env.hs
+++ src/Env.hs
@@ -3,7 +3,17 @@
 {-# LANGUAGE NoImplicitPrelude  #-}
 {-# LANGUAGE OverloadedStrings  #-}
 
-module Env where
+module Env ( EnvironmentException(..)
+           , env
+           , envMaybe
+           , envDecimal
+           , envSignedDecimal
+           , envHexadecimal
+           , envSignedHexademical
+           , envRational
+           , envDouble
+           , envRead
+           ) where
 
 --------------------------------------------------------------------------------
 -- * Internal imports
@@ -17,6 +27,7 @@ import           Control.Monad.Catch    (MonadThrow (..))
 import           Control.Monad.IO.Class
 import           Data.Maybe
 import           Data.Text
+import           Data.Text.Read
 import           Data.Typeable
 import           Prelude
 import           System.Environment     (lookupEnv)
@@ -49,3 +60,43 @@ env key =
 -- | Get value of environment variable.
 envMaybe :: (MonadIO m) => Text -> m (Maybe Text)
 envMaybe key = liftIO $ liftM (pack <$>) (lookupEnv (unpack key))
+
+-- | Get value of environment variable and parse it as integral number. This
+-- function does not handle leading sign character. Use @'envSignedDecimal'@
+-- instead.
+envDecimal :: (MonadIO m, Integral a) => Text -> m (Maybe a)
+envDecimal = envRead decimal
+
+-- | Get value of environment variable and parse it as integral number. This
+-- function handles leading sign character, but works slower than
+-- @'envDecimal'@.
+envSignedDecimal :: (MonadIO m, Integral a) => Text -> m (Maybe a)
+envSignedDecimal = envRead (signed decimal)
+
+-- | Get value of environment variable and parse it as hexadecimal integral
+-- number, consisting of an optional leading "0x". This function does not handle
+-- leading sign character. Use @'envSignedHexademical'@ instead.
+envHexadecimal :: (MonadIO m, Integral a) => Text -> m (Maybe a)
+envHexadecimal = envRead hexadecimal
+
+-- | Get value of environment variable and parse it as hexadecimal integral
+-- number, consisting of an optional leading "0x". This function handles leading
+-- sign character, but works slower than @'envHexadecimal'@.
+envSignedHexademical :: (MonadIO m, Integral a) => Text -> m (Maybe a)
+envSignedHexademical = envRead (signed hexadecimal)
+
+-- | Get value of environment variable and parse it as fractional number.
+envRational :: (MonadIO m, Fractional a) => Text -> m (Maybe a)
+envRational = envRead rational
+
+-- | Get value of environment variable and parse it as double. This function is
+-- almost ten times faster than @'envRational'@, but is slightly less accurate.
+envDouble :: (MonadIO m) => Text -> m (Maybe Double)
+envDouble = envRead double
+
+-- | Get value of environment variable and parse it using specific reader.
+envRead :: (MonadIO m) => Reader a -> Text -> m (Maybe a)
+envRead r = fmap (join . fmap ((fmap fst . fromRight) . r)) . envMaybe
+
+fromRight :: Either a b -> Maybe b
+fromRight = either (const Nothing) Just
